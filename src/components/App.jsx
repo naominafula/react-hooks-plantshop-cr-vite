c406ac458dcd41e9dc7cd94cb469ffefcb3a30c2
naominafula <naomi.nafula@student.moringaschool.com>
2026-05-12 11:08:19 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,19 +6,19 @@ function App() {
   const [plants, setPlants] = useState([]);
   const [search, setSearch] = useState("");
 
-  // Fetch plants when app loads
+  
   useEffect(() => {
     fetch("http://localhost:6001/plants")
       .then((res) => res.json())
       .then((data) => setPlants(data));
   }, []);
 
-  // Add new plant to state
+
   function handleAddPlant(newPlant) {
     setPlants([...plants, newPlant]);
   }
 
-  // Filter plants by search
+  
   const filteredPlants = plants.filter((plant) =>
     plant.name.toLowerCase().includes(search.toLowerCase())
   );
